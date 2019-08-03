b8b8b9eb9334581e162c503fb823c87c3d1c80fc
Mayankho <yankhot96@gmail.com>
2019-08-02 22:35:05 -0400

    Packages installed and project set up

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+
+
+
+const express = require('express');
+const server = express();
+const port = 9000;
+
+const configureMiddleware = require('./config/middleware');
+const projectRouter = require('./projects/projectRouter');
+const actionRouter = require('./actions/actionRouter');
+
+configureMiddleware(server);
+
+server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
+
+server.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+server.listen(port, () => console.log(`\n ==^_^== ${port} ==^_^==`))
